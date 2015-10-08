3a6bf1ae5e95769c8c4afc6613d29d80485a7a41
Darlan Alves <me@darlanalv.es>
2015-10-08 02:09:01 -0300

    0.1.0

--- build.template.js
+++ build.template.js
@@ -1,16 +1,15 @@
 (function(global) {
-	'use strict';
 
-	/* content goes here */
+    /* content goes here */
 
-	if (typeof define === 'function' && define.amd) {
-		define(function() {
-			return Repository;
-		});
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = Repository;
-	} else {
-		global.Repository = Repository;
-	}
+    if (typeof define === 'function' && define.amd) {
+        define(function() {
+            return Repository;
+        });
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Repository;
+    } else {
+        global.Repository = Repository;
+    }
 
 })(this);
--- dist/repository.js
+++ dist/repository.js
@@ -1,7 +1,6 @@
 (function(global) {
-	'use strict';
 
-	'use strict';
+    'use strict';
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
@@ -83,14 +82,14 @@ function mergeObjects(destination, source) {
     return destination;
 }
 
-	if (typeof define === 'function' && define.amd) {
-		define(function() {
-			return Repository;
-		});
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = Repository;
-	} else {
-		global.Repository = Repository;
-	}
+    if (typeof define === 'function' && define.amd) {
+        define(function() {
+            return Repository;
+        });
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Repository;
+    } else {
+        global.Repository = Repository;
+    }
 
 })(this);
